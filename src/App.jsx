0cef0ae65b15b7134efbe52df4d03b71a6e6741b
privilledge <privieroyalty@gmail.com>
2024-01-21 20:01:35 +0200

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,117 @@
+import FaHandPaper from '../public/paper';
+import FaHandRock from '../public/rock';
+import FaHandPeace from '../public/scissors';
+import './App.css'
+import { useState } from 'react';
+
+
+const actions={
+  rock:'scissors',
+  paper:'rock',
+  scissors:'paper'
+};
+const randomAction=()=>{
+
+  const keys=Object.keys(actions);
+  const index=Math.floor(Math.random()*keys.length);
+
+  return keys[index];
+}
+
+const ActionIcon=({action,...props})=>{
+  const icons={
+    rock:FaHandRock,
+    paper:FaHandPaper,
+    scissors:FaHandPeace
+  };
+  const Icon=icons[action];
+  return(
+   <Icon {...props}/>
+  )
+}
+
+const Player=({name='',score=0,action='rock'})=>{
+  return(
+    <div className='player'>  
+    <div className='score'>{`${name} : ${score}`}</div>
+    <div className='action'>
+     {action&& <ActionIcon  action={action}/>}
+      </div>
+  </div>
+  )
+}
+
+const ActionButton=({action='rock',onActionSelected})=>{
+  return(
+    <button className='round-btn' onClick={()=>onActionSelected(action)}>
+      <ActionIcon action={action}/>
+    </button>
+
+  )
+}
+
+const calculateWinner=(action1,action2)=>{
+
+  if(action1===action2){
+    return 0;
+  }else if(actions[action1].includes(action2)){
+    return -1;
+  }else if(actions[action2].includes(action1)){
+    return 1;
+  }
+return null;
+}
+const ShowWinner=({winner=0})=>{
+const text={
+  "-1":"You Win!!",
+  0:"Its a tie",
+  1:"You lose!"
+}
+
+  return(
+<h2>{text[winner]}</h2>
+  )
+}
+
+const App=()=>{
+  const [playerAction,setPlayerAction]=useState('');
+  const [computerAction,setComputerAction]=useState('');
+  const [playerScore,setPlayerScore]=useState(0);
+  const [computerScore,setComputerScore]=useState(0);
+  const [winner,setWinner]=useState(0);
+
+
+  const onActionSelected=(selectedAction)=>{
+    setPlayerAction(selectedAction);
+    const onComputerAction=randomAction();
+    setComputerAction(onComputerAction);
+
+    const newWinner=calculateWinner(selectedAction,onComputerAction);
+    setWinner(newWinner);
+    if(newWinner===-1){
+      setPlayerScore(playerScore+1);
+    }else if(newWinner===1){
+      setComputerScore(computerScore+1)
+    }
+  };
+return(
+  <div className="center">
+  <h1>Rock-Paper-Scissors</h1>
+  <div className='container'>
+  <Player name='Player' score={playerScore}  action={playerAction}/>
+  <Player name='Computer' score={computerScore} action={computerAction}/>
+  <div>
+    </div> 
+    </div>
+    <div className='buttons'>  
+    <ActionButton action='rock' onActionSelected={onActionSelected}/>
+    <ActionButton action='paper' onActionSelected={onActionSelected}/>
+    <ActionButton action='scissors' onActionSelected={onActionSelected}/>
+    </div>
+ <ShowWinner winner={winner}/>
+  
+  </div>
+)
+}
+
+export default App;
